2dec5e5f8645082c799dcde7ecd2713e76068823
victoria santos <vivimarques10@hotmail.com>
2020-03-19 23:05:08 -0300

    Autenticacao firebase codigo

--- app/src/main/java/br/com/victoriasantos/womensafe/CadastroActivity.kt
+++ app/src/main/java/br/com/victoriasantos/womensafe/CadastroActivity.kt
@@ -2,11 +2,57 @@ package br.com.victoriasantos.womensafe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_cadastro.*
 
 class CadastroActivity : AppCompatActivity() {
+
+    val mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
+
+        cadastrobt.setOnClickListener { Cadastrar() }
+
     }
+
+    private fun Cadastrar(){
+        val email = emailet.text.toString()
+        val senha = senhaet.text.toString()
+
+        if(email.isEmpty()){
+            Toast.makeText(this, "Email obrigatório", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if(senha.isEmpty()){
+            Toast.makeText(this, "Senha obrigatória", Toast.LENGTH_LONG).show()
+            return
+        }
+        else{
+            if(senha.length<6){
+                Toast.makeText(this, "Senha precisa ter ao menos 6 caracteres", Toast.LENGTH_LONG).show()
+                return
+            }
+        }
+
+        val operation = mAuth.createUserWithEmailAndPassword(email, senha)
+        operation.addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                Toast.makeText(this, "Usuário cadastrado com sucesso", Toast.LENGTH_LONG).show()
+                finish()
+            }
+            else{
+                val error = task.exception?.localizedMessage
+                    ?: "Não foi possível entrar no aplicativo no momento"
+                Toast.makeText(this, error, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+
+
+
 }
--- app/src/main/java/br/com/victoriasantos/womensafe/LoginActivity.kt
+++ app/src/main/java/br/com/victoriasantos/womensafe/LoginActivity.kt
@@ -1,12 +1,89 @@
 package br.com.victoriasantos.womensafe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    val mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        entrarbt.setOnClickListener { login() }
+        cadastrarbt.setOnClickListener { cadastro() }
+        resetbt.setOnClickListener { ResetarSenha() }
     }
+
+    private fun login(){
+        val email = emailet.text.toString()
+        val senha = senhaet.text.toString()
+
+        if(email.isEmpty()) {
+            Toast.makeText(this, "E-mail obrigatório", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        if(senha.isEmpty()){
+            Toast.makeText(this, "Senha obrigatória", Toast.LENGTH_LONG).show()
+            return
+        }
+        else{
+            if(senha.length < 6){
+                Toast.makeText(this, "Senha precisa ter ao menos 6 caracteres", Toast.LENGTH_LONG).show()
+                return
+            }
+        }
+        // validação no firebase, verificar se senha e email batem
+        val operation = mAuth.signInWithEmailAndPassword(email, senha)
+        operation.addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                val intentMain = Intent(this, MainActivity::class.java)
+                startActivity(intentMain)
+
+            }
+            else {
+                val error = task.exception?.localizedMessage
+                    ?: "Não foi possivel entrar o aplicativo"
+                Toast.makeText(this, error, Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
+    private fun ResetarSenha(){
+
+        if(emailet.text.toString() != ""){
+
+            val operation = mAuth.sendPasswordResetEmail(emailet.text.toString())
+            operation.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "E-mail para recuperar senha enviado", Toast.LENGTH_LONG)
+                        .show()
+
+                } else {
+                    val error = task.exception?.localizedMessage
+                        ?: "Não foi possível enviar o email"
+                    Toast.makeText(this, error, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+        else{
+            Toast.makeText(this, "E-mail vazio", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun cadastro(){
+
+        val intentCadastro = Intent(this, CadastroActivity::class.java)
+        startActivity(intentCadastro)
+
+    }
+
+
 }
