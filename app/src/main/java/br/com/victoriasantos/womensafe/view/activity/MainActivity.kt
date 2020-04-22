d653ff4fee857f753178b308f55a385b59368087
Jennifer Jerônimo <jennifersoares20@hotmail.com>
2020-04-21 21:13:27 -0300

    atualizando

--- app/src/main/java/br/com/victoriasantos/womensafe/view/activity/MainActivity.kt
+++ app/src/main/java/br/com/victoriasantos/womensafe/view/activity/MainActivity.kt
@@ -1,13 +1,20 @@
 package br.com.victoriasantos.womensafe.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.victoriasantos.womensafe.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        quinta_opcao.setOnClickListener{
+            val intentPerfil = Intent(this, ProfileActivity::class.java)
+            startActivity(intentPerfil)
+        }
     }
 }
