f1802062ef955922f251bf86ac834aa946cb29a5
Jennifer Jerônimo <jennifersoares20@hotmail.com>
2020-04-23 19:33:29 -0300

    colocando dados na pagina de infoViolence

--- app/src/main/java/br/com/victoriasantos/womensafe/view/activity/InfoActivity.kt
+++ app/src/main/java/br/com/victoriasantos/womensafe/view/activity/InfoActivity.kt
@@ -1,13 +1,18 @@
 package br.com.victoriasantos.womensafe.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.victoriasantos.womensafe.R
+import kotlinx.android.synthetic.main.activity_info.*
 
 class InfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+
+        info_victim_bt.setOnClickListener {
+            startActivity(Intent(this,ViolenceInfoActivity::class.java)) }
     }
 }
