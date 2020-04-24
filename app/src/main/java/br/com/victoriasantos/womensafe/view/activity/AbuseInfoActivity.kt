f98f88c8185da8ca15bd6984e6f81460c4acba71
Jennifer Jerônimo <jennifersoares20@hotmail.com>
2020-04-23 23:21:12 -0300

    atualizando

--- app/src/main/java/br/com/victoriasantos/womensafe/view/activity/AbuseInfoActivity.kt
+++ app/src/main/java/br/com/victoriasantos/womensafe/view/activity/AbuseInfoActivity.kt
@@ -1,13 +1,33 @@
 package br.com.victoriasantos.womensafe.view.activity
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.victoriasantos.womensafe.R
+import kotlinx.android.synthetic.main.activity_abuse_info.*
 
 class AbuseInfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_abuse_info)
+
+        link1.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://catracalivre.com.br/carnaval-sem-assedio/veja-como-agir-caso-voce-seja-vitima-de-assedio-ou-estupro/")
+            startActivity(intent) }
+
+        link2.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("http://www.mulher.df.gov.br/centro-especializado-de-atendimento-a-mulher-ceam/")
+            startActivity(intent) }
+
+        link3.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://delegaciaeletronica.pcdf.df.gov.br/")
+            startActivity(intent) }
     }
 }
+
+
